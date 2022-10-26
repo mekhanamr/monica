6ce1ff60acb2980b2a09c5d3d25f514920dea2f2
Drisyacv <drisyacv11@gmail.com>
2022-10-26 23:29:47 +0530

    Create Subtract.java
    Subtract in java

---
+++ Subtract.java
@@ -0,0 +1,13 @@
+class Difference{  
+    static int sub(int a,int b)
+    {
+        return a-b;
+    }  
+}
+
+class Subtract{  
+    public static void main(String[] args)
+    {  
+        System.out.println(Difference.sub(21,11));  
+    }
+}
